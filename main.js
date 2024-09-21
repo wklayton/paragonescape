186fd5a454c464d3e0fad8accc8e537830c62e10
wklayton <wklayton@asu.edu>
2024-09-21 11:27:41 -0700

    Got rid of Xola popup and made a checkout page. Redirected gifts to xola page

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
 const faqContainer=document.querySelectorAll(".faq_box"),faqQuestion=document.querySelectorAll(".question");let faqExpValue="false";faqContainer.forEach(e=>{e.addEventListener("click",()=>{e.classList.toggle("active"),faqQuestion.forEach(e=>{faqExpValue=e.parentNode.classList.contains("active")?"true":"false",e.setAttribute("aria-expanded",faqExpValue)})})});
-
-(function() {
-    let co=document.createElement("script");
-    co.type="text/javascript";
-    co.async=true;
-    co.src="https://xola.com/checkout.js";
-    let s=document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(co, s);
-})();
